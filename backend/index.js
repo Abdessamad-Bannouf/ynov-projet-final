790dccdc1043d180a26812dfbae14e5e5f57e713
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-07-02 00:04:13 +0200

    fix: refactor of a few code in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require('../../toto/backend/routes/auth');
-const protectedRoutes = require('../../toto/backend/routes/protected');
-const candidateRoutes = require('../../toto/backend/routes/candidate');
-const calendarRoutes = require('../../toto/backend/routes/calendar')
+const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protected');
+const candidateRoutes = require('./routes/candidate');
 
 const app = express()
 const port = process.env.PORT || 8080;
@@ -16,7 +15,6 @@ app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api', protectedRoutes);
 app.use('/api/candidate', candidateRoutes);
-app.use('/api/calendar', calendarRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
