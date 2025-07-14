8a7ea67ce7e26fd7a598577eaf67177cdaa3a00c
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-07-14 18:28:34 +0200

    feat: setup & use api's google (Oauth + calendar)

---
+++ backend/routes/calendar.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const calendarController = require('../controllers/calendar');
+
+const router = express.Router();
+
+router.get('/login', calendarController.login);
+
+router.get('/oauth2callback', calendarController.loginRedirect);
+
+router.post('/create', calendarController.create);
+
+router.post('/update', calendarController.update);
+
+router.get('/delete', calendarController.delete);
+
+module.exports = router;
