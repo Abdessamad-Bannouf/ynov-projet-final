608dd5553df6ad080221584757fd874c7fe52b63
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-07-18 00:02:45 +0200

    feat: add some crud methods

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -6,9 +6,16 @@ const pagination = require('../middlewares/pagination');
 
 const router = express.Router();
 
-router.post('/', pagination, feedbackController.createFeedback);
-
 router.get('/interview/:interviewId', pagination, feedbackController.getFeedbacksByInterview);
 router.get('/reviewer/:reviewerId', pagination, feedbackController.getFeedbacksByReviewer);
+router.get('/', pagination, feedbackController.showAll);
+
+router.post('/', feedbackController.create);
+
+router.get('/:id', feedbackController.show);
+
+router.put('/:id', feedbackController.update);
+
+router.delete('/:id', feedbackController.delete);
 
 module.exports = router;
