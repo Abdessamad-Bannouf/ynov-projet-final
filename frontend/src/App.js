03487e2eef190592ca78d31a548b60df8c1af487
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-08-01 23:33:18 +0200

    feat: call interviews show api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
             <Route path="/candidates" element={<CandidatesList />} />
             <Route path="/candidates/:id" element={<CandidateDetail />} />
+            <Route path="/interviews" element={<InterviewsList />} />
         </Routes>
       </BrowserRouter>
   );
