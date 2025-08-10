35de81b247463e2ef7f2c1d11605453486f351f7
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-08-10 13:53:06 +0200

    fix: add pagination on query url

--- frontend/src/api/interviews.ts
+++ frontend/src/api/interviews.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = "http://localhost:3000/api";
 
-export const getInterviews = async () => {
-    const res = await axios.get(`${API}/interviews`);
+export const getInterviews = async (page = 1, limit = 10) => {
+    const res = await axios.get(`${API}/interviews?page=${page}&limit=${limit}`);
     return res.data;
 };
 
