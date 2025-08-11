220aeeef1edfbc11c75c62096e63582e3ab33f7c
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-08-11 16:37:53 +0200

    feat: call feedbacks by interview api

--- frontend/src/api/feedbacks.ts
+++ frontend/src/api/feedbacks.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
+const API_URL = "http://localhost:3000/api";
 
-const API_URL = "http://localhost:3000/api/feedbacks";
+const unwrapList = (payload: any) =>
+    Array.isArray(payload) ? payload : (payload?.data ?? []);
 
-
-// GET feedbacks par interview
 export const fetchFeedbacks = async (interviewId: number) => {
-    const res = await axios.get(`${API_URL}?interviewId=${interviewId}`);
-    return res.data;
+    const res = await axios.get(`${API_URL}/feedbacks/interviews/${interviewId}`, {
+        params: { interviewId }
+    });
+    return Array.isArray(res.data) ? res.data : (res.data?.data ?? []);
 };
 
-// POST feedback
 export const createFeedback = async (data: {
     interviewId: number;
     reviewerId: number;
     comments: string;
-
 }) => {
-    const res = await axios.post(`${API_URL}`, data);
-    return res.data;
+    const res = await axios.post(`${API_URL}/feedbacks`, data);
+    return res.data?.data ?? res.data;
 };
