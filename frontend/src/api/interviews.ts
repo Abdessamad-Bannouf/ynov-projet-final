65a9d0e0d11d48a319a2f9d68cbc36f20c6b235a
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2025-08-12 14:32:42 +0200

    fix: add calendarEventId to interview

--- frontend/src/api/interviews.ts
+++ frontend/src/api/interviews.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const API = "http://localhost:3000/api";
 
 export const getInterviews = async (page = 1, limit = 10) => {
-    const res = await axios.get(`${API}/feedbacks/interviews?page=${page}&limit=${limit}`);
-    return res.data;
+    const res = await axios.get(`${API}/interviews?page=${page}&limit=${limit}`);
+    return res.data?.data ?? res.data;
 };
 
 export const createInterview = async (interviewData: {
